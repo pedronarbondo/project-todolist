7279e4e682a0fb583dd68ad7efcaed9b2809141c
Pedro <pedro.narbondo@hotmail.com>
2022-06-21 19:54:02 -0300

    Add strikethrough functionlity, fix stupid mistakes

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import Edit from "../src/img/edit.png";
+import DeleteIcon from "../src/img/trash.png";
+
+function createTaskObject(description, priority, date, project) {
+    class Task {
+        constructor(description, priority, date, project) {
+            this.description = description;
+            this.priority = priority;
+            this.date = `${date.getDate()}-0${date.getMonth()+1}`;
+            this.project = project;
+        }
+    }
+    let task = new Task(description, priority, date, project);
+    return task;
+}
+
+function createTaskUserprompt() {
+    let userDescription = prompt("Description")
+    let userPriority = prompt("Priority");
+    let userDate = new Date();
+    let userProject = "Inbox";
+
+    let userTask = createTaskObject(userDescription, userPriority, userDate, userProject);
+    return userTask
+}
+
+function createUserDom() {
+    let myTask = createTaskUserprompt();
+    let mainDiv = document.getElementById("main");
+
+    /* Task Content */
+    let taskDiv = document.createElement("div");
+    let taskLeft = document.createElement("div");
+    let taskRight = document.createElement("div");
+    let checkbox = document.createElement("input")
+    let taskText = document.createElement("p");
+    let details = document.createElement("button");
+    let date = document.createElement("p");
+    let edit = document.createElement("img");
+    let deleteButton = document.createElement("img");
+
+    /* Task Content classes*/
+    taskDiv.classList.add("task", myTask.priority);
+    taskLeft.classList.add("taskLeft");
+    taskRight.classList.add("taskRight");
+    checkbox.classList.add("isComplete");
+    taskText.classList.add("taskText");
+    details.classList.add("details");
+    date.classList.add("date");
+    edit.classList.add("edit");
+    deleteButton.classList.add("delete")
+
+    /* Text contents*/
+    taskText.textContent = myTask.description;
+    details.textContent = "DETAILS";
+    date.textContent = myTask.date;
+
+    /* Properties */
+    edit.src = Edit;
+    deleteButton.src = DeleteIcon;
+    checkbox.setAttribute("type", "checkbox");
+    
+    /* Appendages */
+    taskRight.append(details, date, edit, deleteButton);
+    taskLeft.append(checkbox, taskText);
+    taskDiv.append(taskLeft, taskRight);
+    mainDiv.append(taskDiv);
+    
+    pressDelete();
+}
+
+function pressAddTask() {
+    let addTask = document.getElementsByClassName("addTask")[0];
+    addTask.onclick= createUserDom;
+}
+
+function pressDelete() {
+    let mainSection = document.getElementById("main");
+    let deleteButton = Array.from(document.getElementsByClassName("delete"));
+    deleteButton.forEach(button => {
+        button.addEventListener("click", function(event) {
+            let target = event.target;
+            let currentTask = target.parentElement.parentElement;
+            mainSection.removeChild(currentTask);
+        })
+    })
+}
+
+pressAddTask();
+
